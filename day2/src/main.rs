b34a1e596144e5b24723835d90638f80ef1f3e94
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-12-04 12:54:22 +0600

    Aoc2021: Add missing files

---
+++ day2/src/main.rs
@@ -0,0 +1,14 @@
+mod part1;
+
+use libaoc21::{AOCDayPart, get_input_for_day_and_part, get_part};
+use crate::part1::Part1;
+
+fn main() {
+    let part = get_part();
+    let input = get_input_for_day_and_part(2, part);
+
+    match part {
+        1 => Part1::new(&input).solve(),
+        _ => panic!("Unknown part!")
+    }
+}
