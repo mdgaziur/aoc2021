a279a7ea089569872b5e1e0704e8ad764ac3e60b
mdgaziur <mdgaziurrahmannoor@gmail.com>
2021-12-04 11:28:32 +0600

    AOC: day1 part1

---
+++ day1/src/part1.rs
@@ -0,0 +1,36 @@
+use libaoc21::fake_stdin::FakeStdin;
+use libaoc21::AOCDayPart;
+use std::io::Read;
+
+pub struct Part1 {
+    stdin: FakeStdin,
+}
+
+impl Part1 {
+    pub fn new(input: &str) -> Self {
+        Self {
+            stdin: FakeStdin::new(input.as_bytes()),
+        }
+    }
+}
+
+impl AOCDayPart for Part1 {
+    fn solve(&mut self) {
+        let mut input = String::new();
+        let mut previous_depth = -1;
+        let mut increased = 0;
+
+        while let Ok(_) = self.stdin.read_to_string(&mut input) {
+            let depth = input.parse::<i32>().expect("Invalid input");
+
+            if previous_depth < depth && previous_depth != -1 {
+                increased += 1;
+            }
+
+            previous_depth = depth;
+            input = String::new();
+        }
+
+        println!("{}", increased);
+    }
+}
---
+++ libaoc21/src/fake_stdin.rs
@@ -0,0 +1,46 @@
+use std::io::{Read, Error, ErrorKind};
+
+pub struct FakeStdin {
+    buf: Vec<u8>,
+    pos: usize,
+}
+
+impl FakeStdin {
+    pub fn new(buf: &[u8]) -> Self {
+        Self {
+            buf: buf.to_vec(),
+            pos: 0,
+        }
+    }
+}
+
+impl Read for FakeStdin {
+    fn read(&mut self, _: &mut [u8]) -> std::io::Result<usize> {
+        unimplemented!()
+    }
+
+    fn read_to_string(&mut self, buf: &mut String) -> std::io::Result<usize> {
+        if self.pos >= self.buf.len() {
+            return Err(Error::new(
+                ErrorKind::UnexpectedEof,
+                "Unexpected end if input",
+            ));
+        }
+
+        // read until new line or eof
+        let mut size = 0;
+
+        while self.pos < self.buf.len() {
+            let ch = char::from(self.buf[self.pos]);
+            self.pos +=  1;
+            if ch.is_whitespace() || ch == '\n' {
+                break;
+            }
+
+            buf.push(ch);
+            size += 1;
+        }
+
+        Ok(size)
+    }
+}
